77fe483e24b1d3931848b7aba9fa9ff7b6f31bdd
AsadAli7789 <155202559+AsadAli7789@users.noreply.github.com>
2024-07-12 08:05:31 -0700

    d

---
+++ app.js
@@ -0,0 +1,8 @@
+var div1 = document.getElementById("lop")
+var div2 = document.getElementById("log")
+var btn1 = document.getElementById("jol")
+btn1.addEventListener('click',pol)
+function pol(){
+    div1.style.display="block"
+    div2.style.display="none"
+}
